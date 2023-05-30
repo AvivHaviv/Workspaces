c85da2e05cbe6269b3cd6d49c8255f950195dbb8
AvivHaviv <avivha@InfraEdge.co.il>
2023-05-30 09:35:21 +0300

    adding devops and qa

---
+++ qa/main.tf
@@ -0,0 +1,12 @@
+for_each = {
+  a_group = "eastus"
+  another_group = "westus2"
+}
+module "workspaces" {
+  for_each = var.users_id
+  source  = "C:/Users/AvivHaviv/modules/terraform-aws-workspaces"
+  workspace_name = each.key
+
+  //source  = "/../../modules/terraform-aws-workspaces"
+  # insert the 2 required variables here
+}
---
+++ qa/variables.tf
@@ -0,0 +1,39 @@
+//user list
+variable "users_id" {
+  description = "The list of the user which the module will create workspaces with."
+  type = list(string)
+}
+
+//vpc
+variable "vpc_id" {
+  description = "id of the vpc"
+  type        = string
+  default = "main_vpc"
+}
+
+variable "vpc_cidr_block" {
+  description = "cidr_block for the vpc"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+//subnet of the qa team
+variable "subnet_cidr_block" {
+  description = "cidr_block for the developers subnet"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+//ip_group of the qa team
+variable "ip_group_source" {
+  description = "cidr_block that define where users will connect from"
+  type        = string
+  default     = "10.10.10.10/16"
+}
+
+//ip_group_contractors of the qa team
+variable "ip_group_contractors_source" {
+  description = "id of the vpc"
+  type        = string
+  default     = "11.11.11.11/16"
+}
